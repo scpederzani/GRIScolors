aed6407364c37ac0d7c3f8b7435b6b18adc050fb
Sarah <sarah.pederzani@gmx.de>
2022-12-05 15:23:36 +0000

    start file to assemble each palette

---
+++ 02_designing_the_palettes/assemble_palettes.R
@@ -0,0 +1,32 @@
+source("02_designing_the_palettes/assemble_functions/assemble_functions.R")
+
+# Gris Logo
+
+Gris <- list("#354B7E", "#6680A3", "#DFE0E1", "#C7A459", "#6E2F35") 
+
+
+# Grey - Denial
+# 
+# Light
+# 
+# dark
+# 
+# Red - Anger
+# 
+# Windswept desert (light)
+# Windmill caverns (dark)
+# 
+# Green - Bargaining
+# 
+# Forest friend (apple, double jump)
+# Black bird
+# 
+# Blue - Depression
+# 
+# Underground tunnels (chasing the star, crystal cave)
+# 
+# Yellow - Acceptance
+# 
+# Turtle of light (eel, the light tree)
+# Light will guide the way
+# Stars and fowls
