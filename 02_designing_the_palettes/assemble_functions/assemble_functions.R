d7b233dcb1a2fac9eff736b6fc6a83c56876414b
Sarah <sarah.pederzani@gmx.de>
2022-12-05 15:23:03 +0000

    add function for displaying palettes

---
+++ 02_designing_the_palettes/assemble_functions/assemble_functions.R
@@ -0,0 +1,12 @@
+
+print.palette <- function(x, ...) {
+  n <- length(x)
+  old <- par(mar = c(0.5, 0.5, 0.5, 0.5))
+  on.exit(par(old))
+  
+  image(1:n, 1, as.matrix(1:n), col = x,
+        ylab = "", xaxt = "n", yaxt = "n", bty = "n")
+  
+  rect(0, 0.92, n + 1, 1.08, col = rgb(1, 1, 1, 0.8), border = NA)
+  text((n + 1) / 2, 1, labels = attr(x, "name"), cex = 2.5, family = "serif")
+}
